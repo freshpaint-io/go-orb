8b42c680e76fb26410745fa80785db73ac762538
Bianchi <m@bianchi.dev>
2026-03-10 17:40:14 -0500

    Make logic a little clearer

--- src/scripts/install.sh
+++ src/scripts/install.sh
@@ -22,10 +22,7 @@ function standard_install {
       echo "Found a different version of Go in /opt/go."
       $SUDO rm -rf /opt/go
     fi
-  fi
-
-  # Download and extract if /opt/go doesn't exist
-  if [ ! -d /opt/go ]; then
+  else
     echo "Installing the requested version of Go."
     curl -O --fail --location -sS "https://freshpaint-cdn.com/binaries/go${ORB_VAL_VERSION}.${OSD_FAMILY}-${HOSTTYPE}.tar.gz"
     $SUDO tar xzf "go${ORB_VAL_VERSION}.${OSD_FAMILY}-${HOSTTYPE}.tar.gz" -C /opt
